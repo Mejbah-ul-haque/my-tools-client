dbcba4b9168a59f39e7e20054fd05aa2c79a9ee3
Mejbah <mejba.s@gmail.com>
2022-09-22 02:36:21 +0600

    color change

--- src/Pages/Home/Business/Business.js
+++ src/Pages/Home/Business/Business.js
@@ -6,25 +6,25 @@ const Business = () => {
 	return (
 		<div className="container mx-auto">
       <div className="stats stats-vertical lg:stats-horizontal shadow ">
-			<div className="stat">
+			<div className="stat text-primary">
       <div className="stat-title text-3xl mb-5"><FontAwesomeIcon icon={faPeoplePulling}></FontAwesomeIcon></div>
 				<div className="stat-value">1000+</div>
 				<div className="stat-desc">customers</div>
 			</div>
-			<div className="stat">
+			<div className="stat text-primary">
       <div className="stat-title text-3xl mb-5"><FontAwesomeIcon icon={faComment}></FontAwesomeIcon></div>
 				<div className="stat-value">31K</div>
 				<div className="stat-desc">Reviews</div>
 			</div>
 
-			<div className="stat">
+			<div className="stat text-primary">
       <div className="stat-title text-3xl mb-5"><FontAwesomeIcon icon={faDollar}></FontAwesomeIcon></div>
 				<div className="stat-value">120M</div>
 				<div className="stat-desc">Anual Revenue</div>
 			</div>
 
-			<div className="stat">
-				<div className="stat-title text-3xl mb-5"><FontAwesomeIcon icon={faTools}></FontAwesomeIcon></div>
+			<div className="stat text-primary">
+				<div className="stat-title text-red-600 text-3xl mb-5"><FontAwesomeIcon icon={faTools}></FontAwesomeIcon></div>
 				<div className="stat-value">50+</div>
 				<div className="stat-desc">Tools</div>
 			</div>
